29dc5385ec6361962a132817ed050c943ad7f11b
DiBDV <bdv.dmitry@gmail.com>
2021-05-31 09:09:52 +0200

    Apply prettier to the script
    Formatted the script with prettier and verified linter.

--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,9 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
-// export const name = () => readlineSync.question('May I have your name please? \n') ;
-
-//const hello = console.log('Hello, ' + name + '!') ;
-
-
-export const getName = () => readlineSync.question('May I have your name? ');
+export const getName = () => readlineSync.question("May I have your name? ");
 export const greating = () => {
-     const name = getName();
-     console.log(`Hello ${name}`);
+  const name = getName();
+  console.log(`Hello, ${name}!`);
 };
-
-
-// export const greating = () => {
-//     const name = readlineSync.question('May I have your name? ');
-//     console.log(`Hello ${name}`);
-// }
