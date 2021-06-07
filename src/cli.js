3c0356c7ea201a4ad66eb2eaeb755953420cf946
DiBDV <bdv.dmitry@gmail.com>
2021-06-07 14:40:49 +0200

    Push latest change in bin and src
    Fixed code in bin/brain-games.js and src/cli.js. Updated version in package-lock.json

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
-#!/usr/bin/env node
+import readlineSync from 'readline-sync';
 
-import readlineSync from "readline-sync";
-
-export const getName = () => readlineSync.question("May I have your name? ");
+export const getName = () => readlineSync.question('May I have your name? ');
 export const greating = () => {
   const name = getName();
   console.log(`Hello, ${name}!`);
