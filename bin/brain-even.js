27d25f0e293ebf72785d689d50bd0e745f1c709c
DiBDV <bdv.dmitry@gmail.com>
2021-06-10 22:43:19 +0200

    Add new game "Even number"
    Added new file brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+// ??? find out, how to make brain-games go first before brain-enen.js ???
+// import "brain-games.js";
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+console.log('Question: ');
+
+console.log('Your answer: ');
+
+console.log('Congratulations, ${name}!')
