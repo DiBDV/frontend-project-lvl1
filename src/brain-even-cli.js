e1483e2a744627798f98cd40cf956ca056d46431
DiBDV <bdv.dmitry@gmail.com>
2021-06-17 22:33:52 +0200

    Add 3 iterations
    Added cycle of 3 iterations to ask the question.

--- src/brain-even-cli.js
+++ src/brain-even-cli.js
@@ -1,18 +1,22 @@
-import readlineSync from 'readline-sync';
-import { getName } from '../src/cli.js';
+import readlineSync from "readline-sync";
+import { getName } from "../src/cli.js";
 
 export const isEven = (num) => {
-  
-console.log(`Question: ${Math.floor(Math.random() * 100)}`);
+  for (let i = 0; i < 3; i += 1) {
+    console.log(`Question: ${Math.floor(Math.random() * 100)}`);
 
-const answer = () => readlineSync.question('Your answer: ');
-
-console.log(`Your answer: ${readlineSync.question("Your answer: ")}`);
-        if (num % 2 === 0 && answer === "yes" && answer !== 'no') {
-        return 'Correct';
-        } if (num % 2 === 0 && answer == 'no') {
-        return `'no' is wrong answer ;(. Correct answer was 'yes'.)
+    let answer = console.log(`Your answer: ${readlineSync.question("Your answer: ")}`);
+    if (answer !== "yes" || answer !== "no") {
+        return 'Please use "yes" or "no" to answer';
+    }
+    if (num % 2 === 0 && answer === "yes" && answer !== "no") {
+      return "Correct!";
+    }
+    if (num % 2 === 0 && answer == "no") {
+      return `'no' is wrong answer ;(. Correct answer was 'yes'.)
         Let's try again, ${getName.name}!`;
-        }
+    }
 
+    console.log(`Congratulations, ${getName.name}`);
+  }
 };
