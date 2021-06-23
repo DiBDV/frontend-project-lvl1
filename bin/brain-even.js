7f194d0b4b2c03799fccc7fd25716f5ba9bb76a8
DiBDV <bdv.dmitry@gmail.com>
2021-06-23 23:15:07 +0200

    Update brain-even
    Another attempt to create logic for the game. I'm fully stuck

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,7 +3,7 @@
 // ??? find out, how to make brain-games go first before brain-enen.js ???
 // import "brain-games.js";
 
-import { isEven } from '../src/brain-even-cli.js';
+import { isEven } from "../src/brain-even-cli.js";
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
--- src/brain-even-cli.js
+++ src/brain-even-cli.js
@@ -1,22 +1,20 @@
 import readlineSync from "readline-sync";
 import { getName } from "../src/cli.js";
 
-export const isEven = (num) => {
-  for (let i = 0; i < 3; i += 1) {
+export const isEven = (number) => number % 2 === 0;
+
+for (let i = 0; i < 3; i += 1) {
+  const question = () =>
     console.log(`Question: ${Math.floor(Math.random() * 100)}`);
+  console.log(question);
 
-    let answer = console.log(`Your answer: ${readlineSync.question("Your answer: ")}`);
-    if (answer !== "yes" || answer !== "no") {
-        return 'Please use "yes" or "no" to answer';
-    }
-    if (num % 2 === 0 && answer === "yes" && answer !== "no") {
-      return "Correct!";
-    }
-    if (num % 2 === 0 && answer == "no") {
-      return `'no' is wrong answer ;(. Correct answer was 'yes'.)
-        Let's try again, ${getName.name}!`;
-    }
+  const userAnswer = () =>
+    console.log(`Your answer: ${readlineSync.question("Your answer: ")}`);
 
-    console.log(`Congratulations, ${getName.name}`);
+  const expectedAnswer = isEven(number) ? "yes" : "no";
+  if (expectedAnswer === userAnswer) {
+    console.log("Correct!");
   }
-};
+
+  console.log(`Congratulations, ${getName.name}`);
+}
