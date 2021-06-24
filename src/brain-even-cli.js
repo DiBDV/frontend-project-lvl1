253e18b4ad0acd7e925bb09324bf238f514a0253
DiBDV <bdv.dmitry@gmail.com>
2021-06-24 23:00:52 +0200

    Update brain-even game logic
    Made the game output 3 times the question and receive some answer.
    Next steps:
    - break once incorrect answer given;
    - receive the name from $getName;
    - ...

--- src/brain-even-cli.js
+++ src/brain-even-cli.js
@@ -4,16 +4,19 @@ import { getName } from "../src/cli.js";
 export const isEven = (number) => number % 2 === 0;
 
 for (let i = 0; i < 3; i += 1) {
-  const question = () =>
-    console.log(`Question: ${Math.floor(Math.random() * 100)}`);
-  console.log(question);
+  const number = Math.floor(Math.random() * 100);
+  console.log(`Question: ${number}`);
 
-  const userAnswer = () =>
-    console.log(`Your answer: ${readlineSync.question("Your answer: ")}`);
+  const userAnswer = readlineSync.question("Your answer: ");
 
   const expectedAnswer = isEven(number) ? "yes" : "no";
   if (expectedAnswer === userAnswer) {
     console.log("Correct!");
+  }
+  if (expectedAnswer !== userAnswer) {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.
+    Let's try again, ${getName.name}`);
+    // return ;
   }
 
   console.log(`Congratulations, ${getName.name}`);
