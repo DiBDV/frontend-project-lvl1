75dcf893224c59a3e9d93a65a909c70f88716b38
DiBDV <bdv.dmitry@gmail.com>
2021-06-25 00:07:06 +0200

    Apply eslint corrections
    Applied eslint corrections for all files

--- src/brain-even-cli.js
+++ src/brain-even-cli.js
@@ -1,17 +1,17 @@
-import readlineSync from "readline-sync";
-import { getName } from "../src/cli.js";
+import readlineSync from 'readline-sync';
+import { getName } from './cli.js';
 
-export const isEven = (number) => number % 2 === 0;
+const isEven = (number) => number % 2 === 0;
 
 for (let i = 0; i < 3; i += 1) {
   const number = Math.floor(Math.random() * 100);
   console.log(`Question: ${number}`);
 
-  const userAnswer = readlineSync.question("Your answer: ");
+  const userAnswer = readlineSync.question('Your answer: ');
 
-  const expectedAnswer = isEven(number) ? "yes" : "no";
+  const expectedAnswer = isEven(number) ? 'yes' : 'no';
   if (expectedAnswer === userAnswer) {
-    console.log("Correct!");
+    console.log('Correct!');
   }
   if (expectedAnswer !== userAnswer) {
     console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.
@@ -19,4 +19,7 @@ for (let i = 0; i < 3; i += 1) {
     break;
   }
 }
+
 console.log(`Congratulations, ${getName.name}`);
+
+export default isEven;
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-export const getName = () => readlineSync.question("May I have your name? ");
+export const getName = () => readlineSync.question('May I have your name? ');
 export const greeting = () => {
   const name = getName();
   console.log(`Hello, ${name}!`);
