10c5bbc16fd4595df9767b643d39702b65f1394b
DiBDV <bdv.dmitry@gmail.com>
2021-10-01 09:28:50 +0200

    Make sure you connect index. js to brain-calc-cli.js
    Updated index.js to accept functions and their parameter from brain-calc-cli.js. Still not working but commiting result to git.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export const random = (min, max) => {
   return Math.floor(Math.random() * ((max + 1) - min) + min);
 };
 
-const startGame = (gameDescription, calculationResult, questionGenerator) => {
+const startGame = (gameDescription, questionGenerator) => {
   const getName = () => readlineSync.question('May I have your name? ');
   const name = getName();
   console.log(`Hello, ${name}!`);
@@ -14,13 +14,14 @@ const startGame = (gameDescription, calculationResult, questionGenerator) => {
 
   // a question will be generated 3 times
   for (let i = 0; i < 3; i += 1) {
-    
-    console.log(`Question: ${questionGenerator.question}`);
+    const userQuestion = questionGenerator(question);
+    console.log(`Question: ${userQuestion}`);
     const userAnswer = readlineSync.question('Your answer: ');
+    const userExpectedAnswer = questionGenerator(expectedAnswer);
 
     // here we're comparing the userAnswer with the right answer
     if (questionGenerator.expectedAnswer !== userAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${userExpectedAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
