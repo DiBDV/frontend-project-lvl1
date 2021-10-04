544d08453625da6c69308dbf16cf659215698759
DiBDV <bdv.dmitry@gmail.com>
2021-10-04 21:10:23 +0200

    Fix linter for utils.js
    Removed curly brackets and 'return' statement to fix the problem.

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const random = (min, max) => { return Math.floor(Math.random() * (max - min + 1) + min); };
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 export default random;
 
 // return Math.floor(Math.random() * ((max + 1) - min) + min);
