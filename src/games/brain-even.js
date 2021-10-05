103730ac7fbc7c1eb067e24f2e3739503dd561c7
DiBDV <bdv.dmitry@gmail.com>
2021-10-05 19:27:09 +0200

    Remove commented out part of the code.

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -7,27 +7,9 @@ const calculationResult = (number) => number % 2 === 0;
 
 export const questionGenerator = () => {
   const number = random(2, 100);
-  const question = number % 2;
+  const question = number;
   const expectedAnswer = calculationResult(number) ? 'yes' : 'no';
   return { question, expectedAnswer };
 };
-
-//   for (let i = 0; i < 3; i += 1) {
-//     const number = Math.floor(Math.random() * 100);
-//     console.log(`Question: ${number}`);
-//     const userAnswer = readlineSync.question('Your answer: ');
-
-//     const expectedAnswer = isEven(number) ? 'yes' : 'no';
-//     if (expectedAnswer === userAnswer) {
-//       console.log('Correct!');
-//     }
-//     if (expectedAnswer !== userAnswer) {
-//       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
-//       console.log(`Let's try again, ${name}!`);
-//       return;
-//     }
-//   }
-//   console.log(`Congratulations, ${name}!`);
-// };
 
 export const runIsEvenGame = () => startGame(gameDescription, questionGenerator);
