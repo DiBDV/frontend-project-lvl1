58c24fa9d3c65e604f4abe43b8022db1bb6e0b83
DiBDV <bdv.dmitry@gmail.com>
2021-10-08 17:48:46 +0200

    Correct the logic of the game
    Corrected the logic of the game to make sure it's calculating correctly.

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -4,20 +4,18 @@ import random from '../utils.js';
 const gameDescription = 'Find the greatest common divisor of given numbers.';
 
 const calculationResult = (number1, number2) => {
-  if (number1 % number2 === 0) {
-    expectedAnswer = number2;
+  if (number2) {
+    return calculationResult(number2, number1 % number2);
   }
-  if (number1 === 1 || number2 === 1) {
-    expectedAnswer = 1;
-  }
+  return Math.abs(number1);
 };
 
 const questionGenerator = () => {
-  const number1 = random(0, 10);
-  const number2 = random(0, 10);
+  const number1 = random(0, 100);
+  const number2 = random(0, 100);
   const question = `${number1} ${number2}`;
   const expectedAnswer = String(calculationResult(number1, number2));
-  return { question, expectedAnswer};
+  return { question, expectedAnswer };
 };
 
 export const runGCDGame = () => startGame(gameDescription, questionGenerator);
