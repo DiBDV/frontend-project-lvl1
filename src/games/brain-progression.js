3a6c0c8cf243e7bcff7cc81ef98fcf34ad8cc608
DiBDV <bdv.dmitry@gmail.com>
2021-10-15 16:47:16 +0200

    Add logic for the game.

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -6,14 +6,16 @@ const gameDescription = 'What number is missing in the progression?';
 const calculationResult = (arrayFirstNumber, arrayCoefficient) => {
   const array = [];
   array.length = 10;
-  const arrayElement = array[arrayFirstNumber + (array.length - 1) * arrayCoefficient];
-  
+  const arrayElements = array[arrayFirstNumber + (array.length - 1) * arrayCoefficient];
+  console.log(arrayElements);
 };
 
 const questionGenerator = () => {
   const arrayFirstNumber = random(0,10);
   const arrayCoefficient = random(0,10);
-
+  const question = calculationResult.arrayElements;
+  const expectedAnswer = String(calculationResult(arrayFirstNumber, arrayCoefficient));
+  return { question, expectedAnswer };
 };
 
 const runProgressionGeame = () => startGame(gameDescription, questionGenerator);
