3f7c2264c7cdd3455f877cf4eb5b6423bf2120da
DiBDV <bdv.dmitry@gmail.com>
2021-11-01 20:59:33 +0100

    Introduce new game for prime numbers
    Introduced new game brain-prime.js

---
+++ src/games/brain-prime.js
@@ -0,0 +1,22 @@
+import startGame from '../index.js';
+import random from '../utils.js';
+
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
+
+const questionGenerator = () => {
+  const num = random(2, 100);
+  const question = num;
+  const expectedAnswer = String(isPrime(num)) ? 'yes' : 'no';
+  return { question, expectedAnswer };
+};
+
+const runPrimeGame = () => startGame(gameDescription, questionGenerator);
+
+export default runPrimeGame;
